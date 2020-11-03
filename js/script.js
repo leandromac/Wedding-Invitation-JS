1ecf039b56cb9922271d3786916d32d120348466
leandromac <contato@leandromacedo.com>
2020-11-03 20:54:33 -0300

    Use strict and iife

--- js/script.js
+++ js/script.js
@@ -1,50 +1,55 @@
-const titleConfirm = document.querySelector('.titleConfirm')
-const titleGift = document.querySelector('.titleGift')
-const bodyConfirm = document.querySelector('.bodyConfirm')
-const bodyGift = document.querySelector('.bodyGift')
-const modalFooter = document.querySelector('.modal-footer')
+(function(){
+  'use strict'
+  
+  const titleConfirm = document.querySelector('.titleConfirm')
+  const titleGift = document.querySelector('.titleGift')
+  const bodyConfirm = document.querySelector('.bodyConfirm')
+  const bodyGift = document.querySelector('.bodyGift')
+  const modalFooter = document.querySelector('.modal-footer')
 
-$('#btn-submit').click(e => {
-  e.preventDefault()
-  firebase.database().ref("/" + Math.floor(Math.random() * 50)).set({
-    name: $('#name').val()
+  $('#btn-submit').click(e => {
+    e.preventDefault()
+    firebase.database().ref("/" + Math.floor(Math.random() * 50)).set({
+      name: $('#name').val()
+    })
+    $('.close').click()
+    notification()
+    let name = $('#name').val()
+    $('.guestName').text(name)
+    $('#name').val('')
   })
-  $('.close').click()
-  notification()
-  let name = $('#name').val()
-  $('.guestName').text(name)
-  $('#name').val('')
-})
-                
-$('.btnConfirm').click(e => {
-  e.preventDefault()
-  hideItem(titleGift)
-  hideItem(bodyGift)
-  showItem(titleConfirm)
-  showItem(bodyConfirm)
-  showItem(modalFooter)
-})
+                  
+  $('.btnConfirm').click(e => {
+    e.preventDefault()
+    hideItem(titleGift)
+    hideItem(bodyGift)
+    showItem(titleConfirm)
+    showItem(bodyConfirm)
+    showItem(modalFooter)
+  })
 
-$('.btnGift').click(e => {
-  e.preventDefault()
-  hideItem(titleConfirm)
-  hideItem(bodyConfirm)
-  hideItem(modalFooter)
-  showItem(titleGift)
-  showItem(bodyGift)
-})
+  $('.btnGift').click(e => {
+    e.preventDefault()
+    hideItem(titleConfirm)
+    hideItem(bodyConfirm)
+    hideItem(modalFooter)
+    showItem(titleGift)
+    showItem(bodyGift)
+  })
 
-function showItem(item) {
-  item.style.display = 'block'
-}
+  function showItem(item) {
+    item.style.display = 'block'
+  }
 
-function hideItem(item) {
-  item.style.display = 'none'
-}
+  function hideItem(item) {
+    item.style.display = 'none'
+  }
 
-function notification() {
-  $('.alert').show(1000)
-  setInterval(() => {
-    $('.alert').hide(1000)
-  }, 5000)
-}
+  function notification() {
+    $('.alert').show(1000)
+    setInterval(() => {
+      $('.alert').hide(1000)
+    }, 5000)
+  }
+
+})()
