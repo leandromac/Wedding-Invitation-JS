a42a680fc854c38bb9e6c979dd8e834705c6cab5
leandromac <contato@leandromacedo.com>
2020-11-03 20:45:05 -0300

    Refatoring to jquery

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-const button = document.querySelector('#btn-submit')
 const titleConfirm = document.querySelector('.titleConfirm')
 const titleGift = document.querySelector('.titleGift')
 const bodyConfirm = document.querySelector('.bodyConfirm')
 const bodyGift = document.querySelector('.bodyGift')
 const modalFooter = document.querySelector('.modal-footer')
-var database = firebase.database()
 
-button.addEventListener('click', e => {
+$('#btn-submit').click(e => {
   e.preventDefault()
-  database.database().ref("/" + Math.floor(Math.random() * 50)).set({
-    name: newGuest()
+  firebase.database().ref("/" + Math.floor(Math.random() * 50)).set({
+    name: $('#name').val()
   })
-  console.log(newGuest())              
+  $('.close').click()
+  notification()
+  let name = $('#name').val()
+  $('.guestName').text(name)
+  $('#name').val('')
 })
-
+                
 $('.btnConfirm').click(e => {
   e.preventDefault()
   hideItem(titleGift)
@@ -32,14 +34,17 @@ $('.btnGift').click(e => {
   showItem(bodyGift)
 })
 
-function newGuest() {
-  return document.getElementById("name").value
-}
-
 function showItem(item) {
   item.style.display = 'block'
 }
 
 function hideItem(item) {
   item.style.display = 'none'
+}
+
+function notification() {
+  $('.alert').show(1000)
+  setInterval(() => {
+    $('.alert').hide(1000)
+  }, 5000)
 }
