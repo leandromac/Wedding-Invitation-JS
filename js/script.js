ebf52d773d31ed5c0cfffdddb32ff05b9e52c22c
leandromac <contato@leandromacedo.com>
2020-11-03 17:35:49 -0300

    Modal Gift

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyDSIlQzhjPDSWecwlE4x8TgwQr2kfc7LCc",
-  authDomain: "wedding-invitation-62ab8.firebaseapp.com",
-  databaseURL: "https://wedding-invitation-62ab8.firebaseio.com",
-  projectId: "wedding-invitation-62ab8",
-  storageBucket: "wedding-invitation-62ab8.appspot.com",
-  messagingSenderId: "1014859546602",
-  appId: "1:1014859546602:web:c66b50a322813a5f2e3c93"
-};
-firebase.initializeApp(firebaseConfig);
-
 const button = document.querySelector('#btn-submit')
+const titleConfirm = document.querySelector('.titleConfirm')
+const titleGift = document.querySelector('.titleGift')
+const bodyConfirm = document.querySelector('.bodyConfirm')
+const bodyGift = document.querySelector('.bodyGift')
+const modalFooter = document.querySelector('.modal-footer')
+var database = firebase.database()
 
 button.addEventListener('click', e => {
-  e.preventDefault
+  e.preventDefault()
   database.database().ref("/" + Math.floor(Math.random() * 50)).set({
     name: newGuest()
   })
   console.log(newGuest())              
 })
 
+$('.btnConfirm').click(e => {
+  e.preventDefault()
+  hideItem(titleGift)
+  hideItem(bodyGift)
+  showItem(titleConfirm)
+  showItem(bodyConfirm)
+  showItem(modalFooter)
+})
+
+$('.btnGift').click(e => {
+  e.preventDefault()
+  hideItem(titleConfirm)
+  hideItem(bodyConfirm)
+  hideItem(modalFooter)
+  showItem(titleGift)
+  showItem(bodyGift)
+})
+
 function newGuest() {
   return document.getElementById("name").value
+}
+
+function showItem(item) {
+  item.style.display = 'block'
+}
+
+function hideItem(item) {
+  item.style.display = 'none'
 }
